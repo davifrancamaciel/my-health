11e2b0e3e76dc9a4466e655497994d29ede6a6a1
Davi França Maciel <davifrancamaciel@hotmail.com>
2021-08-05 10:53:30 -0300

    atualização de dashboard em tempo real

--- frontend/src/pages/Dashboard/AppointmentsLineGraph/index.jsx
+++ frontend/src/pages/Dashboard/AppointmentsLineGraph/index.jsx
@@ -1,94 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { Line } from 'react-chartjs-2';
-import numeral from 'numeral';
-import { formatPrice } from 'Utils/formatPrice';
-import api from 'services/api';
-import { PRIMARY_COLOR, SECONDARY_COLOR } from 'constants/colors';
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { Line } from 'react-chartjs-2'
+import numeral from 'numeral'
+import { formatPrice } from 'Utils/formatPrice'
+import api from 'services/api'
+import { PRIMARY_COLOR, SECONDARY_COLOR } from 'constants/colors'
 
 const options = {
-	legend: {
-		display: false,
-	},
-	elements: {
-		point: {
-			radius: 0,
-		},
-	},
-	maintainAspectRatio: false,
-	tooltips: {
-		mode: 'index',
-		intersect: false,
-		callbacks: {
-			label: function (tooltipItem, data) {
-				return formatPrice(tooltipItem.value);
-			},
-		},
-	},
-	scales: {
-		xAxes: [
-			{
-				type: 'time',
-				time: {
-					tooltipFormat: 'DD/MM/YYYY', //''ll',
-				},
-			},
-		],
-		yAxes: [
-			{
-				gridLines: {
-					display: false,
-				},
-				ticks: {
-					// Include a dollar sign in the ticks
-					callback: function (value, index, values) {
-						return numeral(value).format('0a');
-					},
-				},
-			},
-		],
-	},
-};
-
-const buildChartData = (data) => {
-	return data.map((item) => ({
-		y: Number(item.value),
-		x: item.date,
-	}));
-};
-
-function LineGraph({ ...props }) {
-	const [data, setData] = useState({});
-
-	useEffect(() => {
-		async function load() {
-			const response = await api.get('dashboard/appointments-graph');
-			let dataFormated = buildChartData(response.data);
-			setData(dataFormated);
-		}
-		load();
-	}, []);
-
-	return (
-		<div>
-			{data?.length > 0 && (
-				<div className={props.className}>
-					<h3>Receita de agendamentos no ultimo ano</h3>
-					<Line
-						data={{
-							datasets: [
-								{
-									backgroundColor: `${PRIMARY_COLOR}`,
-									borderColor: 'rgb(47,135,191)',
-									data: data,
-								},
-							],
-						}}
-						options={options}
-					/>
-				</div>
-			)}
-		</div>
-	);
+  legend: {
+    display: false
+  },
+  elements: {
+    point: {
+      radius: 0
+    }
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: 'index',
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return formatPrice(tooltipItem.value)
+      }
+    }
+  },
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          tooltipFormat: 'DD/MM/YYYY' //''ll',
+        }
+      }
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          // Include a dollar sign in the ticks
+          callback: function (value, index, values) {
+            return numeral(value).format('0a')
+          }
+        }
+      }
+    ]
+  }
 }
 
-export default LineGraph;
+const buildChartData = data => {
+  return data.map(item => ({
+    y: Number(item.value),
+    x: item.date
+  }))
+}
+
+function LineGraph ({ ...props }) {
+  const notificationsList = useSelector(state => state.notification.list)
+  const profile = useSelector(state => state.user.profile)
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  useEffect(() => {
+    const [notification] = notificationsList
+    if (notification.provider_id === profile.id && !notification.read) {
+      load()
+    }
+  }, [notificationsList])
+
+  async function load () {
+    const response = await api.get('dashboard/appointments-graph')
+    let dataFormated = buildChartData(response.data)
+    setData(dataFormated)
+  }
+  return (
+    <div>
+      {data?.length > 0 && (
+        <div className={props.className}>
+          <h3>Receita de agendamentos no ultimo ano</h3>
+          <Line
+            data={{
+              datasets: [
+                {
+                  backgroundColor: `${PRIMARY_COLOR}`,
+                  borderColor: 'rgb(47,135,191)',
+                  data: data
+                }
+              ]
+            }}
+            options={options}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default LineGraph
--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,57 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import { useSelector } from 'react-redux';
-import api from 'services/api';
+import { useSelector } from 'react-redux'
+import api from 'services/api'
 
-import Container from 'components/_layouts/Container';
+import Container from 'components/_layouts/Container'
 //import SignatureControl from './SignatureControl';
-import CardContainer from './CardContainer';
-import AppointmentsLineGraph from './AppointmentsLineGraph';
+import CardContainer from './CardContainer'
+import AppointmentsLineGraph from './AppointmentsLineGraph'
 
-import { HeaderContainer, DashboardContainer } from './styles';
-import getValidationErrors from 'Utils/getValidationErrors';
-import history from 'services/browserhistory';
+import { HeaderContainer, DashboardContainer } from './styles'
+import getValidationErrors from 'Utils/getValidationErrors'
+import history from 'services/browserhistory'
 
 const Dashboard = () => {
-	const profile = useSelector((state) => state.user.profile);
-	const [dashboard, setDashboard] = useState({});
-	const [loaded, setLoaded] = useState(false);
+  const notificationsList = useSelector(state => state.notification.list)
+  const profile = useSelector(state => state.user.profile)
+  const [dashboard, setDashboard] = useState({})
+  const [loaded, setLoaded] = useState(false)
 
-	useEffect(() => {
-		if (!profile.provider) {
-			history.push('/appointment');
-		} else {
-			loadDashboard();
-		}
-	}, []);
+  useEffect(() => {
+    if (!profile.provider) {
+      history.push('/appointment')
+    } else {
+      loadDashboard()
+    }
+  }, [])
 
-	async function loadDashboard() {
-		try {
-			const response = await api.get('dashboard');
-			setDashboard(response.data);
-			setLoaded(true);
-		} catch (error) {
-			getValidationErrors(error);
-		}
-	}
-	return (
-		<>
-			<HeaderContainer>
-				<div>
-					{/* {!profile.company_provider && loaded && (
+  useEffect(() => {
+    const [notification] = notificationsList
+    if (notification.provider_id === profile.id && !notification.read) {
+      loadDashboard();
+    }
+  }, [notificationsList])
+
+  async function loadDashboard () {
+    try {
+      const response = await api.get('dashboard')
+      setDashboard(response.data)
+      setLoaded(true)
+    } catch (error) {
+      getValidationErrors(error)
+    }
+  }
+  return (
+    <>
+      <HeaderContainer>
+        <div>
+          {/* {!profile.company_provider && loaded && (
             <SignatureControl company={dashboard.company} />
           )} */}
-				</div>
-			</HeaderContainer>
-			<Container>
-				<DashboardContainer>
-					<CardContainer  dashboard={dashboard} loaded={loaded} />
+        </div>
+      </HeaderContainer>
+      <Container>
+        <DashboardContainer>
+          <CardContainer dashboard={dashboard} loaded={loaded} />
 
-					<AppointmentsLineGraph className="appointment-graph" />
-				</DashboardContainer>
-			</Container>
-		</>
-	);
-};
+          <AppointmentsLineGraph className='appointment-graph' />
+        </DashboardContainer>
+      </Container>
+    </>
+  )
+}
 
-export default Dashboard;
+export default Dashboard

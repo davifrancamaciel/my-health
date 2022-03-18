f5fb0a66c4f9d4f18858f0afd85e10af2d9cdcba
Davi Maciel <davi.maciel@nagra.com>
2022-03-18 08:36:10 -0300

    ajuste de layout home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,13 +5,26 @@ import { Link } from 'react-router-dom'
 
 import logo from 'assets/logoupish.png';
 
-import { Page, Container, Buttons } from './styles';
+import { Page, Container, Content, Buttons } from './styles';
 
-function Home() {
+
+const Home = () => {
+
+    const Buttom = ({ path, title, icon }) => {
+        return (
+            <Link to={`/${path}`}>
+                <span>
+                    {icon}
+                </span>
+                <strong>{title}</strong>
+            </Link>
+        )
+    }
+
     return (
         <Page>
             <Container>
-                <div className='content'>
+                <Content>
                     <header>
                         <img src={logo} alt='UPIS Saúde' />
                     </header>
@@ -21,28 +34,12 @@ function Home() {
                             A plataforma UPIS SAÚDE foi criada para levar o seu atendimento no seu bolso, facilitando a busca de profissionais de saúde, com uma forma atual e prática de contratar e agendar consultas, exames e procedimentos.
                         </p>
                         <Buttons>
-
-                            <Link to={'/login'}>
-                                <span>
-                                    <FiLogIn />
-                                </span>
-                                <strong>Acessar minha conta</strong>
-                            </Link>
-                            <Link to={'/register'}>
-                                <span>
-                                    <FiUser />
-                                </span>
-                                <strong>Criar conta como paciente</strong>
-                            </Link>
-                            <Link to={'/register-provider'}>
-                                <span>
-                                    <AiFillMedicineBox />
-                                </span>
-                                <strong>Criar conta como pestador de serviços</strong>
-                            </Link>
+                            <Buttom title={'Acessar minha conta'} path={'login'} icon={<FiLogIn />} />
+                            <Buttom title={'Criar conta como paciente'} path={'register'} icon={<FiUser />} />
+                            <Buttom title={'Criar conta como pestador de serviços'} path={'register-provider'} icon={<AiFillMedicineBox />} />
                         </Buttons>
                     </main>
-                </div>
+                </Content>
             </Container>
         </Page>
     )
--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -13,49 +13,47 @@ export const Container = styled.div`
   max-width: 1200px;
   background: #fff url(${bgUp}) no-repeat right;
   background-size: 450px;
-  color: var(--text-color);
+  color: var(--text-color); 
+`;
 
-  .content {
-    width: 100%;
-    height: 100%;
-    max-width: 1100px;
-    margin: 0 auto;
-    padding: 0 30px;
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 30px;
 
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 
-  .content header {
+  header {
     margin: 48px 0 0;
   }
 
-  .content header img {
+  header img {
     max-width: 200px;
   }
 
-  .content main {
+  main {
     flex: 1;
-    /* max-width: 560px; */
-
     display: flex;
     flex-direction: column;
     justify-content: center;
   }
 
-  .content main h1 {
+  main h1 {
     font-size: 44px;
     color: var(--title-color);
   }
 
-  .content main p {
+  main p {
     font-size: 24px;
     margin-top: 24px;
     line-height: 38px;
   }
 
-  .content main a {
+   main a {
     width: 100%;
     max-width: 360px;
     height: 72px;
@@ -70,7 +68,7 @@ export const Container = styled.div`
     margin-top: 40px;
   }
 
-  .content main a span {
+   main a span {
     display: block;
     background: rgba(0, 0, 0, 0.08);
     width: 72px;
@@ -82,45 +80,48 @@ export const Container = styled.div`
     transition: background-color 0.2s;
   }
 
-  .content main a span svg {
+   main a span svg {
     color: #fff;
     width: 20px;
     height: 20px;
   }
 
-  .content main a strong {
+   main a strong {
     flex: 1;
     text-align: center;
     color: #fff;
   }
 
-  .content main a:hover {
+   main a:hover {
     background: #2fb86e;
   }
 
-  @media (max-width: 900px) {
-    .content {
-      align-items: center;
-      text-align: center;
-    }
+  @media (max-width: 900px) {     
+    align-items: center;
+    text-align: center;    
 
-    .content header {
+    header {
       margin: 48px auto 0;
     }
 
-    .content main {
+    main {
       align-items: center;
     }
 
-    .content main h1 {
-      font-size: 42px;
+    main h1 {
+      font-size: 25px;
     }
 
-    .content main p {
-      font-size: 24px;
+    main p {
+      font-size: 18px;
     }
   }
-`;
+  @media (max-width: 550px) {     
+    main {
+      margin-top:25px;
+    }
+  }
+`
 
 export const Buttons = styled.div`
 
@@ -132,6 +133,7 @@ export const Buttons = styled.div`
   grid-gap: 32px;
   @media (max-width: 780px) {
     grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
   }
   @media (max-width: 530px) {
     grid-template-columns: repeat(1, 1fr);
